d269006e57a4331a31582939dc4328638bc4cf1a
rayzen2002 <emanuelzsmj@gmail.com>
2025-12-23 22:02:10 -0300

    fix: remove @vercel/analytics to unblock Vercel CVE scan

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,6 @@ import { ThemeProvider } from "@/components/theme/theme-provider";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,8 +41,6 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
-            <SpeedInsights />
-            <Analytics />
           </ThemeProvider>
         </body>
       </html>
