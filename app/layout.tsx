3244a20a1555fffc295a2ba325743b7e3141c27b
rayzen2002 <emanuelzsmj@gmail.com>
2026-06-07 01:14:23 -0300

    feat: relatorio de fechamento automatico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,10 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Toaster } from "sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -29,7 +17,6 @@ export default function RootLayout({
     <QueryClientProvider client={queryClient}>
       <html
         lang="en"
-        className={cn(geistSans.variable, geistMono.variable)}
         suppressHydrationWarning
       >
         <body className="antialiased">
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'drizzle-kit'
 import 'dotenv/config';
 
-console.log(process.env.DATABASE_URL)
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
